fc35f7cab47b4acb274e80a5b1a2d77bbe56ae4f
Davidthesoul <davidthesoul@hotmail.com>
2021-03-01 19:28:25 -0300

    actualizacion

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const bcrypt = require('bcrypt');
 //const mysql = require("mysql");
 //const util = require ("util");
 
+
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(express.json());
+
 
 const auth = (req, res, next) =>{
   try{
@@ -19,35 +21,37 @@ const auth = (req, res, next) =>{
 
       if(!token){
         throw new Error("No estas logueado");
-        
-
       }
        token =token.replace("Bearer", "")
-      jwt.verify(token, "Secret", (err,user) =>{
-        if (err){
-          throw new Error("Token inválido");
-          }
+       jwt.verify(token, "Secret", (err,user) =>{
+       if (err){
+          throw new Error("Token inválido");}
+        
         //finaliza recconocimiento
+        
         });
-        next();
-      }
-      catch(e){
-        console.error(e.message);
+        next();   
+    }
+    catch(e){
+      console.error(e.message);
       res.status(413).send({message: e.message});
       }
+}
 
-  }
-auth.unless=unless;
-
+//auth.unless=unless;
+/*
 
   app.use(auth.unless({
     path: [
       {url: "/login", methods: ["POST"]},
-      {URL: "/registro", methods: ["POST"]}
-    ]
+      {url: "/registro", methods: ["POST"]}
+          ]
   }));
+*/
+ // app.use(auth);
 
-  app.use(auth);
+
+
 //pasos para crear el acceso a sesion
 //Paso 1: Registro
 app.post("/registro", async(req, res)=>
@@ -56,7 +60,7 @@ app.post("/registro", async(req, res)=>
         //valido que me mande correctamente la información
        if (!req.body.usuario || !req.body.clave || !req.body.email  || !req.body.celu){
          throw new Error("No enviaste todos los datos necesarios");
-       }
+        }
        //select * FROM usuario WHERE usuario = req.body.usuario;
         //Si el usuario existe mando error
 
@@ -74,10 +78,6 @@ app.post("/registro", async(req, res)=>
 
        res.send({message: "Se registro correctamente"});
 
-       
-
-
-    
       }
       catch(e){
         console.error(e.message);
@@ -87,18 +87,13 @@ app.post("/registro", async(req, res)=>
 
 });
 
-
-
 //Paso 2: Login
-
-
 app.post("/login",(req, res)=>
 {
     try {
         if (!req.body.usuario || !req.body.clave){
            throw new Error("No enviaste todos los datos necesarios");
         }
-       
         //Paso 2-a Encuetro el usuario en la base de datos
         //select * FROM usuario WHERE usuario = req.body.usuario;
         //si no encuetro el usuario - error
@@ -108,7 +103,6 @@ app.post("/login",(req, res)=>
        // if(!bcrypt.compareSync(req.body.clave, ClaveEncriptada)){
        //     throw new Error("Fallo en login")
        // }
-
         //paso 2-c inicio sesion
         const tokenData = {
             nombre: "David",
@@ -119,35 +113,24 @@ app.post("/login",(req, res)=>
             expiresIn: 60 * 60 * 24 //Expira en 24 horas
         })
         res.send({token});
-
-
        }    
       catch(e){
         console.error(e.message);
         res.status(413).send({message: e.message});
         
       }
-
 });
 
 
 app.get("/libros",(req, res)=>{
   try { 
- 
-        res.send({message: "Bienvenido!"});
-  
+   res.send({message: "Bienvenido!"});
   }
   catch(e){
-    console.error(e.message);
-    res.status(413).send({message: e.message});
-    
+   console.error(e.message);
+   res.status(413).send({message: e.message});   
   }
-
 });
-
-
-//permite recibir en javascript todo lo que reciba json
-//const PATH = require('path');
 
 //empleo el puerto 3000 para la comunicación con el server
 app.listen(port, ()=> {
@@ -155,30 +138,3 @@ app.listen(port, ()=> {
     });
 
 
-
-//agrego el body-parse para facilitar el post
-//var body_parser = require("body-parser");
-//app.use(body_parser.urlencoded({extended: false}));
-//app.use(body_parser.json());
-
-
-
-
-//base get o post sin async
-
-/*
-app.get("/otrosd",(req, res)=>
-{
-    try {
-
-    
-      }
-      catch(e){
-        console.error(e.message);
-        res.status(413).send({message: e.message});
-        
-      }
-
-});
-
-*/
